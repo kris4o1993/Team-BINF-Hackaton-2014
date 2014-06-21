e23b3d25128ca1471689509a5613690f9943f54d
epitsin <epitsin@yahoo.com>
2014-06-21 15:53:40 +0300

    some unnecessary things deleted

--- project/Binf/scripts/people.js
+++ project/Binf/scripts/people.js
@@ -1,31 +1,4 @@
-﻿var people = [{
-    nickname: 'epitsin',
-    gender: 'Female',
-    age: '21',
-    interests: 'Programming, Basketball, Travelling, Belot'
-}, {
-    nickname: 'adzhazhev',
-    gender: 'Male',
-    age: '22',
-    interests: 'Programming, Football, Computer Games, Belot'
-}, {
-    nickname: 'kris4o1993',
-    gender: 'Male',
-    age: '21',
-    interests: 'Programming, Game of thrones, Computer Games, Belot'
-}, {
-    nickname: 'chervenkov',
-    gender: 'Female',
-    age: '23',
-    interests: 'Programming, Sport, Computer Games, Belot'
-}, {
-    nickname: 'pesho',
-    gender: 'Male',
-    age: '65',
-    interests: 'Pedophilia, Little children, BDSM'
-}];
-
-window.onload = function () {
+﻿window.onload = function () {
     var people = [{
         nickname: 'epitsin',
         gender: 'Female',
@@ -56,16 +29,6 @@ window.onload = function () {
     var peopleListContainer = document.getElementById('target-info');
 
     var peopleListTemplate = Handlebars.compile((document.getElementById('people-template')).innerHTML);
-
-    //// empty the container
-    //while (authorsListContainer.firstChild) {
-    //    authorsListContainer.removeChild(authorsListContainer.firstChild);
-    //}
-
-    //authorsListContainer.innerHTML = authorsListTemplate({
-    //    authors: authors
-    //});
-
 
     var addPeople = peopleListTemplate( people[0] );
 
