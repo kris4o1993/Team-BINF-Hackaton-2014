31a33509b02822e27fe926a18d28fc0cd739aa9e
Radoslav Chervenkov <chervenkovr@gmail.com>
2014-06-22 10:02:51 +0300

    updated coordinates, added event handlers for the event info

--- project/Binf/scripts/mapsAPI.js
+++ project/Binf/scripts/mapsAPI.js
@@ -1,10 +1,51 @@
+$('#event-info').hide();
+
+var people = [{
+    activity: 'volleyball',
+    from: '09:00',
+    to: '11:00'
+}, {
+    activity: 'basketball',
+    from: '10:00',
+    to: '11:30'
+}, {
+    activity: 'football',
+    from: '09:00',
+    to: '11:00'
+}, {
+    activity: 'chess',
+    from: '18:00',
+    to: '19:00'
+}, {
+    activity: 'belot',
+    from: '19:00',
+    to: '21:00'
+}, {
+    activity: 'running',
+    from: '08:00',
+    to: '09:00'
+}, {
+    activity: 'walking dogs',
+    from: '20:00',
+    to: '21:00'
+}];
+//
+//var peopleListContainer = document.getElementById('target-info');
+//
+//var peopleListTemplate = Handlebars.compile((document.getElementById('people-template')).innerHTML);
+//
+//var addPeople = peopleListTemplate( people[0] );
+//
+//peopleListContainer.innerHTML += addPeople;
+
 function success(position) {
-    var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    var coords2 = new google.maps.LatLng(position.coords.latitude + 0.001, position.coords.longitude + 0.003);
-    var coords3 = new google.maps.LatLng(position.coords.latitude - 0.003, position.coords.longitude - 0.001);
+    var mapCenter = new google.maps.LatLng(42.685591, 23.341881);
+    var coordsFirstEvent = new google.maps.LatLng(mapCenter.lat() - 0.003, mapCenter.lng() - 0.001);
+    var coordsSecondEvent = new google.maps.LatLng(mapCenter.lat() + 0.003, mapCenter.lng() - 0.004);
+    var coordsThirdEvent = new google.maps.LatLng(mapCenter.lat() - 0.001, mapCenter.lng() - 0.005);
 
     var mapOptions = {
-        center: coords,
+        center: mapCenter,
         zoom: 15
     };
 
@@ -12,22 +53,24 @@ function success(position) {
         mapOptions);
 
     var marker = new google.maps.Marker({
-        position: coords,
+        position: coordsFirstEvent,
         map: map,
         title: "We are coding here!"
     });
 
     var marker2 = new google.maps.Marker({
-        position: coords2,
+        position: coordsSecondEvent,
         map: map,
         title: "We are coding here!"
     });
 
     var marker3 = new google.maps.Marker({
-        position: coords3,
+        position: coordsThirdEvent,
         map: map,
         title: "We are coding here!"
     });
+
+    console.log(marker3);
 
     //var markers
 
@@ -36,7 +79,24 @@ function success(position) {
     });
 
     google.maps.event.addListener(marker, 'click', function () {
-        infowindow.open(map, marker);
+        $('#event-info').show();
+        $('#target-main-info').text(people[0]['activity']);
+        $('#target-from').text(people[0]['from']);
+        $('#target-to').text(people[0]['to']);
+    });
+
+    google.maps.event.addListener(marker2, 'click', function () {
+        $('#top-menu').show();
+        $('#target-main-info').text(people[1]['activity']);
+        $('#target-from').text(people[1]['from']);
+        $('#target-to').text(people[1]['to']);
+    });
+
+    google.maps.event.addListener(marker3, 'click', function () {
+        $('#top-menu').show();
+        $('#target-main-info').text(people[2]['activity']);
+        $('#target-from').text(people[2]['from']);
+        $('#target-to').text(people[2]['to']);
     });
 }
 
